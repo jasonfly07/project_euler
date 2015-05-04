1396b0ff5afd5baf0d6a7e8f40fa6bf0355328eb
jasonfly07 <shengchieh1989@gmail.com>
2015-05-03 19:42:47 -0700

    comments & cleanup

--- p_031_coin_sums.cpp
+++ p_031_coin_sums.cpp
@@ -1,38 +1,36 @@
+// This problem is essentially solving 
+// a + 2b + 5c + 10d + 20e + 50f + 100g + 200h = 200
+// a~h are integers >= 0
+
+// Can be done with recursion. 
+// TODO: dynamic programming?
 #include <iostream>
-#include <string>
 #include <vector>
 #include <cmath>
 using namespace std;
 
-template<typename T>
-void PrintVector(vector<T>& v);
-
 int SumCombination(int sum, vector<int> coeffs) {
   // Base case
   if(coeffs.size() == 1){
-    cout << "Base case: " << sum << "," << coeffs[0] << endl;
     if( sum % coeffs[0] == 0 ) return 1;
     else                       return 0;
   }
 
   // Recursive case
+  // Retrieve the largest coefficient
   int currCoeff = coeffs.back();
   coeffs.pop_back();
   
   int numCombinations = 0;
+  // iMax = maximum number of currCoeff that can contribute to sum
   int iMax = static_cast<int>( floor( sum / static_cast<double>(currCoeff) ) );
 
-    for( int i = 0; i <= iMax; i++ ){
+  for( int i = 0; i <= iMax; i++ ){
     int partialSum = sum - i*currCoeff;
-    // cout << partialSum << endl;
 
     if( partialSum == 0 ) numCombinations++;
     else if( partialSum > 0 ){
-      cout << "Calling SumCombination(" << partialSum << ","; 
-      PrintVector(coeffs);
-      cout << ")" << endl;
       int partialSolution = SumCombination(partialSum, coeffs);
-      cout << "Return " << partialSolution << endl;
       numCombinations += partialSolution;
 
     }
@@ -41,18 +39,8 @@ int SumCombination(int sum, vector<int> coeffs) {
   return numCombinations;
 }
 
-template<typename T>
-void PrintVector(vector<T>& v) {
-  cout << "{ ";
-  for(T t: v){
-    cout << t << ",";
-  }
-  cout << "}";
-}
-
 int main() {
   vector<int> coeffs{1,2,5,10,20,50,100,200};
-  // vector<int> coeffs{1,2,5};
 
   int solution = SumCombination(200,coeffs);
   cout << solution << endl;
